58b167b6b6fd1f813fad8d3bb098f21c8ddba5ac
AlexBarbachov <66849795+AlexBarbachov@users.noreply.github.com>
2025-09-20 18:34:10 -0700

    second

--- Lab11.cpp
+++ Lab11.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <string>
 
-struct musicPlaylist
+using namespace std;
+
+
+// Will store the name, artists in the playlist, and some song names
+// Dynamic array will be the list of songs.
+struct playlist
 {
+    string name;
+    string *songs;
 
+    // size of the array
+    int numSongs;
 };
 
 int main()
+{
+
+}
+
+
+/* 
+will add a song to the playlist and allocate more memory if needed
+Done through deleting array and creating a new one with more size
+*/
+void addSong(playlist &playl)
 {
 
 }
